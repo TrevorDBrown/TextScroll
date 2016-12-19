e7a38387cc683d420a425d2545c0d512420d5449
Trevor D. Brown <trevor.brown593@topper.wku.edu>
2016-12-19 00:45:16 -0600

    Add existing files

---
+++ TextScroll/TextScroll/LeftToRightScrollForm.Designer.vb
@@ -0,0 +1,58 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class LeftToRightScrollForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.TextLabel = New System.Windows.Forms.Label()
+        Me.SlideTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'TextLabel
+        '
+        Me.TextLabel.AutoSize = True
+        Me.TextLabel.Location = New System.Drawing.Point(13, 22)
+        Me.TextLabel.Name = "TextLabel"
+        Me.TextLabel.Size = New System.Drawing.Size(39, 13)
+        Me.TextLabel.TabIndex = 0
+        Me.TextLabel.Text = "Label1"
+        '
+        'SlideTimer
+        '
+        Me.SlideTimer.Interval = 10
+        '
+        'LeftToRightScrollForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(384, 62)
+        Me.Controls.Add(Me.TextLabel)
+        Me.Name = "LeftToRightScrollForm"
+        Me.Text = "TextScroll Example"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextLabel As Label
+    Friend WithEvents SlideTimer As Timer
+End Class
---
+++ TextScroll/TextScroll/LeftToRightScrollForm.vb
@@ -0,0 +1,58 @@
+﻿' TextScroll - A simple text scroll example for Visual Basic.NET applications.
+' Initial Developer: Trevor D. Brown.
+
+' This code is distributed under the MIT License. Please distribute as you wish.
+
+' LeftToRightScrollForm -   This form demonstrates a text scroll which moves left to right, and pauses once the text label reaches the left-most
+'                           edge of the form. After 100 ticks, the movement continues.
+
+Public Class LeftToRightScrollForm
+
+    ' Declare variables to be used throughout the form.
+    Dim CurrentPauseCount As Integer            ' Integer to serve as a counter for the pause count.
+    Const MaximumPauseCount As Integer = 100    ' Constant Integer to represent the maximum pause count.
+    Const TickRate As Integer = 1               ' Constant Integer to represent the rate in which the timer ticks (1 = 1 millisecond)
+    Dim IsPaused As Boolean                     ' Boolean to represent the pause state (True = Paused, False = Not Paused)
+
+
+    ' MainForm_Load - The event handler for when the form initally loads.
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextLabel.Text = "This text scrolls from left to right, and pauses for 100 timer ticks when the left edge reaches the left side of the form."   ' Sets some text within the label.
+        TextLabel.Location = New System.Drawing.Point(0, 0)    ' Positions the text label in the center of the form vertically, while placing it on the left edge of the form.
+        CurrentPauseCount = 0   ' Sets the Current Pause Counter to 0.
+        IsPaused = True         ' Sets the IsPaused Boolean Flag to True.
+        SlideTimer.Interval = TickRate  ' Sets the tick rate of the timer.
+        SlideTimer.Start()    ' Starts the text slide timer.
+    End Sub
+
+    ' SliderTimer_Tick - The event handler for when the specific timer ticks.
+    Private Sub SlideTimer_Tick(sender As Object, e As EventArgs) Handles SlideTimer.Tick
+        ' Pause conditions
+        If (IsPaused = True And CurrentPauseCount < MaximumPauseCount) Then
+            ' If the text slide is paused, and the current count is less than the maximum allowed count,
+            ' increment the pause count, and remain paused.
+            CurrentPauseCount += 1
+        ElseIf (IsPaused = True And CurrentPauseCount >= MaximumPauseCount) Then
+            ' If the text slide is paused, and the current count is equal to or exceeds the maximum allowed count,
+            ' change the IsPaused flag to False, shift the text label to the left by 1 pixel, and reset the pause count.
+            IsPaused = False
+            TextLabel.Left -= 1
+            CurrentPauseCount = 0
+        ElseIf (IsPaused = False And TextLabel.Right > 0 And TextLabel.Left <> 0) Then
+            ' If the text slide is not paused, and the label's right-most edge is still visible,
+            ' shift the text leabel to the left by 1 pixel.
+            TextLabel.Left -= 1
+        ElseIf (IsPaused = False And TextLabel.Right <= 0) Then
+            ' If the text slide is not paused, and the label's right-most edge is not visible,
+            ' reset the position of the text label to the far right of the form.
+            TextLabel.Left = Me.Width
+        End If
+        ' This statement will only evaluate properly when it is separated from the rest of the If-ElseIf statement.
+        If (IsPaused = False And TextLabel.Left = 0) Then
+            ' If the text slide is not paused, and the label's left-most edge is at X = 0 (relative to the parent form),
+            ' change the IsPaused flag to True, increment the pause count, and pause.
+            IsPaused = True
+            CurrentPauseCount += 1
+        End If
+    End Sub
+End Class
